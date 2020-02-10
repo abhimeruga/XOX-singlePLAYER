b427f4959c14f5a6c9146a82562d07f9f89ccadb
abhimeruga <abhimad94@gmail.com>
2020-02-10 14:53:05 +0530

    environment changes for XOX multiplayer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCmq4WDfU1SmLYFRqQ9iAAcZsCJzTe3hvo',
+    authDomain: 'xox-game-8f298.firebaseapp.com',
+    databaseURL: 'https://xox-game-8f298.firebaseio.com',
+    projectId: 'xox-game-8f298',
+    storageBucket: 'xox-game-8f298.appspot.com',
+    messagingSenderId: '561813140781',
+    appId: '1:561813140781:web:fe28751cc2f77487294fa4'
+  }
 };
